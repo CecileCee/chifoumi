28f71f4f264b60ca21de3a3f852965cbc4ddb3e3
CecileCee <cecile.muloway@gmail.com>
2016-03-20 22:35:01 +0100

    maj fichiers: couleurs en css via DOM

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var myArray = ['Pierre', 'Feuille', 'Ciseaux']; // créer un tableau, qui contie
 var myBtn = document.getElementById('btnGo');
 var score = document.getElementById('affiche');
 var ratioV = document.getElementById('pourcentage');
+var bgColor = document.querySelectorAll("body, select, input");
 var countLost = 0; // compteur victoire ordinateur
 var countWin = 0; // compteur victoire user
 
@@ -12,24 +13,53 @@ var goClick = function() { // fonction appelée seulement par le clic de btnGo
     console.log(choiceComputer)
     console.log(entreeUser);
 
+    //Couleurs pour la défaite, la victoire et l'égalité
+    var bgColorRed = function() {
+      for (var item = 0; item < bgColor.length; item++) {
+        bgColor[item].style.backgroundColor = '#cb3e3e'; // parcours le tableau de querySelectorAll
+        console.log(item);
+      }
+    }
+
+    var bgColorGreen = function() {
+      for (var item = 0; item < bgColor.length; item++) {
+        bgColor[item].style.backgroundColor = '#30962b';
+        console.log(item);
+      }
+    }
+
+    var bgColorNeutral = function() {
+      for (var item = 0; item < bgColor.length; item++) {
+        bgColor[item].style.backgroundColor = '#999d9b';
+        console.log(item);
+      }
+    }
+
   if (entreeUser === choiceComputer) { // égalité
+    bgColorNeutral();
     alert("Egalité : l'ordinateur a aussi choisi " + choiceComputer);
   } else if (entreeUser === 'Ciseaux' && choiceComputer === 'Pierre') {
+    bgColorRed();
     countLost++;
     alert(choiceComputer + " gagne, +1 pour l'ordinateur !"); // l'ordinateur gagne
   } else if (entreeUser === 'Feuille' && choiceComputer === 'Ciseaux') {
+    bgColorRed();
     countLost++;
     alert(choiceComputer + " gagne, +1 pour l'ordinateur !"); // l'ordinateur gagne
   } else if (entreeUser === 'Pierre' && choiceComputer === 'Feuille') {
+    bgColorRed();
     countLost++;
     alert(choiceComputer + " gagne, +1 pour l'ordinateur !"); // l'ordinateur gagne
   } else if (choiceComputer === 'Ciseaux' && entreeUser === 'Pierre') {
+    bgColorGreen();
     countWin++;
     alert(entreeUser + " gagne, +1 pour toi !"); // l'utilisateur gagne
   } else if (choiceComputer === 'Feuille' && entreeUser === 'Ciseaux') {
+    bgColorGreen();
     countWin++;
     alert(entreeUser + " gagne, +1 pour toi !"); // l'utilisateur gagne
   } else if (choiceComputer === 'Pierre' && entreeUser === 'Feuille') {
+    bgColorGreen();
     countWin++;
     alert(entreeUser + " gagne, +1 pour toi !"); // l'utilisateur gagne
   }
